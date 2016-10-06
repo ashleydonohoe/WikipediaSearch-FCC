59739041674182eff0183973e0e96afc639661b3
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-10-06 00:24:11 -0400

    Working on alignment of search group

--- script.js
+++ script.js
@@ -22,15 +22,12 @@ $("#search").click(function() {
 });
 
 function showOutput(resultsArray) {
-    console.log(resultsArray);
     if(resultsArray.length == 0) {
         $("#results-block").append("<h3 class='text-center'>Sorry, no results found</h3>");
     } else {
         for (item in resultsArray) {
-            console.log(resultsArray[item]);
             var titleName = resultsArray[item].title;
             var wikipediaLink = "<h3><a target='_blank' href='https://en.wikipedia.org/wiki/" + titleName + "'>" + titleName + "</a></h3>";
-            console.log(wikipediaLink);
             var snippet = "<p>" + resultsArray[item].snippet + "<p>";
             var hr = "<hr>";
             var HTMLtoAppend = wikipediaLink + snippet + hr;
